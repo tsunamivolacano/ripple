00f1328a564de52542ec52713acafdfbbf40e543
tsunamivolacano <shanniddhya@gmail.com>
2026-08-13 21:48:54 +0530

    Fix import statements in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,10 @@
+import "./faro";  
+import { createRoot } from "react-dom/client";  
+import App from "./App.tsx";  
+import "./globals.css";  
+
+createRoot(document.getElementById("root")!).render();  
+
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./globals.css";
