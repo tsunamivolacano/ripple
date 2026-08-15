91dff12af752d65ad8ccf5185d6109a1aa0a2f88
Dyad <shanniddhya@gmail.com>
2026-08-15 23:18:13 +0530

    Reverted all changes back to version 21c6eec2a2216cf92f163eebee59f55eab797dfb

--- supabase/migrations/0003_add_trigger_to_auto_update_profiles_updated_at.sql
+++
@@ -1,4 +0,0 @@
-DROP TRIGGER IF EXISTS update_profiles_updated_at ON public.profiles;
-CREATE TRIGGER update_profiles_updated_at
-  BEFORE UPDATE ON public.profiles
-  FOR EACH ROW EXECUTE FUNCTION public.update_updated_at_column();
