8426e24ec21829dd3fe6ac2b65ae58a8acf68e73
Dyad <shanniddhya@gmail.com>
2026-08-20 19:44:18 +0530

    wrote 2 file(s)

--- src/context/RippleContext.tsx
+++ src/context/RippleContext.tsx
@@ -76,6 +76,7 @@ interface RippleContextType {
   // Auth actions
   loginWithEmail: (email: string, password: string) => Promise<AuthResponse>;
   signUpWithEmail: (email: string, password: string) => Promise<AuthResponse>;
+  resendConfirmationEmail: (email: string) => Promise<boolean>;
   loginDemoAccount: (personaId: string) => void;
   logout: () => void;
 
@@ -148,6 +149,7 @@ export const RippleProvider: React.FC<{ children: React.ReactNode }> = ({ childr
         currentUser: auth.currentUser,
         loginWithEmail: auth.loginWithEmail,
         signUpWithEmail: auth.signUpWithEmail,
+        resendConfirmationEmail: auth.resendConfirmationEmail,
         loginDemoAccount: auth.loginDemoAccount,
         logout: auth.logout,
 
